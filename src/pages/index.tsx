b22fe9b35d9486c15585f6b1423324cd4f8e6382
Afsoon <said.atrahouch@gmail.com>
2020-01-09 14:32:34 +0100

    Content of home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,50 +33,50 @@ const Header = () => {
         </button>
       </div>
       <nav
-        className={`h-12 justify-between p-1 flex-full ${display} md:flex md:justify-around  md:flex-1 lg:flex lg:flex-col lg:justify-center`}
+        className={`h-12 justify-between p-4 flex-full ${display} md:flex md:justify-around  md:flex-1 lg:flex lg:flex-col lg:justify-center`}
       >
         <a
           href="/"
-          className="group self-center flex items-center lg:flex-col lg:p-4"
+          className="group self-center flex items-center flex-col sm:flex-row lg:flex-col pt-2 lg:p-4"
         >
           <IconHome />
-          <span className="pl-3 lg:pl-0 group-hover:text-support-positive-800">
+          <span className="sm:pl-3 lg:pl-0 group-hover:text-support-positive-800">
             Home
           </span>
         </a>
         <a
           href="/"
-          className="group self-center flex items-center lg:flex-col lg:p-4"
+          className="group self-center flex items-center flex-col sm:flex-row lg:flex-col pt-2 lg:p-4"
         >
           <IconUser />
-          <span className="pl-3 lg:pl-0 group-hover:text-support-positive-800">
+          <span className="sm:pl-3 lg:pl-0 group-hover:text-support-positive-800">
             About
           </span>
         </a>
         <a
           href="/"
-          className="group self-center flex items-center lg:flex-col lg:p-4"
+          className="group self-center flex items-center flex-col sm:flex-row lg:flex-col pt-2 lg:p-4"
         >
           <IconGraphBar />
-          <span className="pl-3 lg:pl-0 group-hover:text-support-positive-800">
+          <span className="sm:pl-3 lg:pl-0 group-hover:text-support-positive-800">
             Skills
           </span>
         </a>
         <a
           href="/"
-          className="group self-center flex items-center lg:flex-col lg:p-4"
+          className="group self-center flex items-center flex-col sm:flex-row lg:flex-col pt-2 lg:p-4"
         >
           <IconBuilding />
-          <span className="pl-3 lg:pl-0 group-hover:text-support-positive-800">
+          <span className="sm:pl-3 lg:pl-0 group-hover:text-support-positive-800">
             Experience
           </span>
         </a>
         <a
           href="/"
-          className="group self-center flex items-center lg:flex-col lg:p-4"
+          className="group self-center flex items-center flex-col sm:flex-row lg:flex-col pt-2 lg:p-4"
         >
           <IconBriefcase />
-          <span className="pl-3 lg:pl-0 group-hover:text-support-positive-800">
+          <span className="sm:pl-3 lg:pl-0 group-hover:text-support-positive-800">
             Projects
           </span>
         </a>
@@ -93,10 +93,115 @@ const Header = () => {
   )
 }
 
+const HomeContent = () => (
+  <div className="bg-primary-600 flex-full text-white flex flex-wrap pt-10 pl-4 text-4xl lg:text-6xl lg:p-16 lg:items-center">
+    <div className="flex flex-col">
+      <div>
+        <span className="hover:text-support-positive-800 cursor-default">
+          H
+        </span>
+        <span className="hover:text-support-positive-800 cursor-default">
+          i
+        </span>
+        <span className="hover:text-support-positive-800 cursor-default">
+          ,
+        </span>
+      </div>
+      <div>
+        <span className="hover:text-support-positive-800 cursor-default">
+          I
+        </span>
+        <span className="hover:text-support-positive-800 cursor-default">
+          '
+        </span>
+        <span className="hover:text-support-positive-800 cursor-default">
+          m
+        </span>
+        &nbsp;
+        <span className="hover:text-support-positive-800 cursor-default">
+          S
+        </span>
+        <span className="hover:text-support-positive-800 cursor-default">
+          a
+        </span>
+        <span className="hover:text-support-positive-800 cursor-default">
+          i
+        </span>
+        <span className="hover:text-support-positive-800 cursor-default">
+          d
+        </span>
+        <span className="hover:text-support-positive-800 cursor-default">
+          ,
+        </span>
+      </div>
+      <div>
+        <span className="hover:text-support-positive-800 cursor-default">
+          S
+        </span>
+        <span className="hover:text-support-positive-800 cursor-default">
+          o
+        </span>
+        <span className="hover:text-support-positive-800 cursor-default">
+          f
+        </span>
+        <span className="hover:text-support-positive-800 cursor-default">
+          t
+        </span>
+        <span className="hover:text-support-positive-800 cursor-default">
+          w
+        </span>
+        <span className="hover:text-support-positive-800 cursor-default">
+          a
+        </span>
+        <span className="hover:text-support-positive-800 cursor-default">
+          r
+        </span>
+        <span className="hover:text-support-positive-800 cursor-default">
+          e
+        </span>
+        &nbsp;
+        <span className="hover:text-support-positive-800 cursor-default">
+          D
+        </span>
+        <span className="hover:text-support-positive-800 cursor-default">
+          e
+        </span>
+        <span className="hover:text-support-positive-800 cursor-default">
+          v
+        </span>
+        <span className="hover:text-support-positive-800 cursor-default">
+          e
+        </span>
+        <span className="hover:text-support-positive-800 cursor-default">
+          l
+        </span>
+        <span className="hover:text-support-positive-800 cursor-default">
+          o
+        </span>
+        <span className="hover:text-support-positive-800 cursor-default">
+          p
+        </span>
+        <span className="hover:text-support-positive-800 cursor-default">
+          e
+        </span>
+        <span className="hover:text-support-positive-800 cursor-default">
+          r
+        </span>
+        <span className="hover:text-support-positive-800 cursor-default">
+          .
+        </span>
+      </div>
+      <span className="text-lg text-primary-400">
+        Front End Developer / Back End Developer
+      </span>
+    </div>
+  </div>
+)
+
 const Home = () => (
-  <div className="h-screen w-screen flex flex-col lg:flex-row">
+  <div className="h-screen w-screen overflow-hidden flex flex-col lg:flex-row">
     <Header />
-    <div className="bg-primary-500 flex-full">CONTENT</div>
+    <HomeContent />
   </div>
 )
 
