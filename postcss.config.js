d88b20eb3e485e3c47ce75a7a6a676798f20deb1
Afsoon <said.atrahouch@gmail.com>
2020-01-15 15:06:08 +0100

    remove purgecss dependency unnecesary

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,11 @@ module.exports = {
     require("tailwindcss"),
     ...(process.env.NODE_ENV === `production`
       ? [
-          
+          require("@fullhuman/postcss-purgecss")({
+            content: ["./src/**/*.js"],
+            defaultExtractor: content =>
+              content.match(/[A-Za-z0-9-_:/]+/g) || []
+          }),
           require("autoprefixer"),
           require("cssnano")
         ]
