e4ab0251abd6a6feff2431276a8d3875ad1f1900
Afsoon <said.atrahouch@gmail.com>
2020-01-15 06:44:52 +0100

    final spanish texts

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -102,131 +102,14 @@ const AboutContent = () => (
         funcional.
       </p>
       <p className="pt-4">
-        Persona resolutiva y detallista. Fan de la calistenía, de actividades al
-        aire libre, de viajar para descubrir nuevas culturas, series de TV.
-        Actualmente en busca de nuevas oportunidades labolares
+        Persona resolutiva, detallista y colaboradora . Interesado por la
+        calistenia, el culturismo y otras actividades al aire libre. Apasionado
+        por viajar, cocinar y ver series de TV.
       </p>
       <p className="pt-4">
-        Interesado tanto por el desarrollo front como back.
+        Actualmente, en busca de nuevas oportunidades laborales. Atraído tanto
+        por el desarrollo front como por el back.
       </p>
-    </div>
-    <div className="flex-3/10 pb-4">
-      <h2 className="text-3xl text-support-success-050 pl-2">
-        Laboral Experience
-      </h2>
-      <ol className="pl-4">
-        <li className="pt-4 list-inside ">
-          <span className="text-2xl block">Software Developer - Ride On</span>
-          <span className="text-sm block text-primary-400">
-            June 2017 - May 2019, Madrid
-          </span>
-          <p className="pt-4 pl-2 text-primary-300">
-            El primer mes empece con la{' '}
-            <span className="font-bold text-white">
-              primera versión para el sistema de reporte de incidencias
-            </span>{' '}
-            de la plataforma. Desarrollado con
-            <span className="font-bold text-white"> Clojure</span>,
-            <span className="font-bold text-white"> ClojureScript</span>,
-            <span className="font-bold text-white"> PostgreSQL</span>.
-          </p>
-          <p className="pt-4 pl-2 text-primary-300">
-            A continuación empece con{' '}
-            <span className="font-bold text-white">
-              el sistema de notificaciones
-            </span>{' '}
-            de la plataforma que requería una{' '}
-            <span className="font-bold text-white">
-              integración con Firebase
-            </span>
-            . Desarrollado con{' '}
-            <span className="font-bold text-white"> Clojure</span>,
-            <span className="font-bold text-white"> Firebase</span>,
-            <span className="font-bold text-white"> PostgreSQL</span>.
-          </p>
-          <p className="pt-4 pl-2 text-primary-300">
-            <span className="font-bold text-white">
-              Mejora del rendimiento de la plataforma​
-            </span>{' '}
-            dado que había problemas para guardar el estado de las estaciones.
-            El <span className="font-bold text-white">problema​</span> se
-            encontraba en que ​
-            <span className="font-bold text-white">
-              se guardaba el histórico de estados
-            </span>{' '}
-            de una base​. La{' '}
-            <span className="font-bold text-white">solución</span> a la​ que se
-            llegó fue usar
-            <span className="font-bold text-white"> Kinesis</span>,
-            <span className="font-bold text-white"> AWS Lambda</span>,
-            <span className="font-bold text-white"> Redis</span>.
-          </p>
-          <p className="pt-4 pl-2 text-primary-300">
-            <span className="font-bold text-white">
-              Desarrollo​ de una aplicación web​
-            </span>{' '}
-            ​y​ de un{' '}
-            <span className="font-bold text-white">
-              ​sistema de gestión de incidencias​
-            </span>{' '}
-            para los trabajadores de mantenimiento del producto. El{' '}
-            <span className="font-bold text-white">sistema de incidencias</span>{' '}
-            se diseño teniendo en mente{' '}
-            <span className="font-bold text-white">
-              integrarse con Pagerduty
-            </span>
-            . Desarrollado con
-            <span className="font-bold text-white"> Kotlin</span>,
-            <span className="font-bold text-white"> Arrow</span>,
-            <span className="font-bold text-white"> Spring Boot</span>,
-            <span className="font-bold text-white"> Javascript</span>,
-            <span className="font-bold text-white"> React</span>,
-            <span className="font-bold text-white"> Clojure</span>,
-            <span className="font-bold text-white"> ​Elasticsearch​</span>,
-            <span className="font-bold text-white"> SNS</span>,
-            <span className="font-bold text-white"> SQS</span>,
-            <span className="font-bold text-white"> Keycloak</span>,
-            <span className="font-bold text-white"> PagerDuty</span>,
-            <span className="font-bold text-white"> PostgreSQL</span>.
-          </p>
-        </li>
-        <li className="pt-4">
-          <span className="text-2xl block">
-            Programador Junior- Indizen Technology & Business Confluence
-          </span>
-          <span className="text-sm block text-primary-400">
-            January 2016 - November 2016, Madrid
-          </span>
-          <p className="pt-4 pl-2 text-primary-300">
-            <span className="font-bold text-white">
-              Ayudar en el desarrollo de una ​aplicación web de sanidad para
-              ayudar a los codificadores​
-            </span>{' '}
-            a detectar el diagnóstico, actual y previos de un informe médico y
-            todos los procedimientos que se han realizado. Desarrollado con
-            <span className="font-bold text-white"> Java 7</span>,
-            <span className="font-bold text-white"> MongoDB</span>,
-            <span className="font-bold text-white"> Spring</span>,
-            <span className="font-bold text-white"> Tomcat</span>,
-            <span className="font-bold text-white"> Javascript</span>,
-            <span className="font-bold text-white"> jQuery</span>.
-          </p>
-          <p className="pt-4 pl-2 text-primary-300">
-            En los últimos seis meses estuve en un proyecto que consistía en la
-            ​
-            <span className="font-bold text-white">
-              modernización del sistema de facturación
-            </span>{' '}
-            de una empresa energética​. Desarrollado con
-            <span className="font-bold text-white"> Java 5</span>,
-            <span className="font-bold text-white"> Javascript</span>,
-            <span className="font-bold text-white"> OracleDB</span>,
-            <span className="font-bold text-white"> WebLogic</span>,
-            <span className="font-bold text-white"> Toplink</span>,
-            <span className="font-bold text-white"> Thymleaf</span>.
-          </p>
-        </li>
-      </ol>
     </div>
     <div className="flex-3/10 pb-4">
       <h2 className="text-3xl text-support-success-050 pl-2">Education</h2>
@@ -239,10 +122,11 @@ const AboutContent = () => (
             September 2012 - Actual, Mostoles
           </span>
           <p className="pt-4 pl-2 text-primary-200">
-            Actualmente me encuentro desarrollando el proyecto de fin de carrera
-            que consiste en el desarrollo de dos aplicaciones web para la
-            gestión de una web. Los primeros meses fueron formación con los
-            cursos de FrontendMasters y los dos últimos desarrollo.
+            En este momento, me encuentro desarrollando el proyecto de fin de
+            grado, que consiste en el desarrollo de dos aplicaciones web para la
+            gestión de una peluqueria. Los primeros meses los dediqué a formarme
+            con los cursos de FrontendMasters y los dos últimos los estoy
+            empleando en el desarrollo.
           </p>
         </li>
         <li className="pt-4">
@@ -251,13 +135,15 @@ const AboutContent = () => (
             October 2015 - December 2015, Madrid
           </span>
           <p className="pt-4 pl-2 text-primary-200">
-            Immersive code retreat to improve your coding skills making an open
-            source project. The project that was developed is
-            <a href="/"> Game Voting</a>
+            Code retreat para mejorar mis habilidad de programación y soft
+            skills realizando un proyecto open source. Durante estos 9 meses se
+            desarrolló el juego <a href="/">Game Voting</a>.
           </p>
         </li>
       </ul>
-      <h3 className="text-2xl text-support-success-050 pl-2 pt-4">Languages</h3>
+    </div>
+    <div className="flex-3/10 pb-4">
+      <h2 className="text-2xl text-support-success-050 pl-2 pt-4">Languages</h2>
       <ul className="pl-4">
         <li className="pt-4">
           <span className="text-2xl">Spanish -</span>
@@ -269,7 +155,7 @@ const AboutContent = () => (
         </li>
       </ul>
       <h3 className="text-2xl text-support-success-050 pl-2 pt-4">
-        Other Course
+        Self Study
       </h3>
       <ul className="pl-4">
         <li className="pt-4 text-2xl">
--- src/pages/experience.tsx
+++ src/pages/experience.tsx
@@ -102,71 +102,77 @@ const ExperienceContent = () => (
         <span className="text-sm block text-primary-400">
           June 2017 - May 2019, Madrid
         </span>
-        <p className="pt-4 pl-2 text-primary-300">
-          El primer mes empece con la{' '}
-          <span className="font-bold text-white">
-            primera versión para el sistema de reporte de incidencias
-          </span>{' '}
-          de la plataforma. Desarrollado con
-          <span className="font-bold text-white"> Clojure</span>,
-          <span className="font-bold text-white"> ClojureScript</span>,
-          <span className="font-bold text-white"> PostgreSQL</span>.
-        </p>
-        <p className="pt-4 pl-2 text-primary-300">
-          A continuación empece con{' '}
-          <span className="font-bold text-white">
-            el sistema de notificaciones
-          </span>{' '}
-          de la plataforma que requería una{' '}
-          <span className="font-bold text-white">integración con Firebase</span>
-          . Desarrollado con{' '}
-          <span className="font-bold text-white"> Clojure</span>,
-          <span className="font-bold text-white"> Firebase</span>,
-          <span className="font-bold text-white"> PostgreSQL</span>.
-        </p>
-        <p className="pt-4 pl-2 text-primary-300">
-          <span className="font-bold text-white">
-            Mejora del rendimiento de la plataforma​
-          </span>{' '}
-          dado que había problemas para guardar el estado de las estaciones. El{' '}
-          <span className="font-bold text-white">problema​</span> se encontraba
-          en que ​
-          <span className="font-bold text-white">
-            se guardaba el histórico de estados
-          </span>{' '}
-          de una base​. La{' '}
-          <span className="font-bold text-white">solución</span> a la​ que se
-          llegó fue usar
-          <span className="font-bold text-white"> Kinesis</span>,
-          <span className="font-bold text-white"> AWS Lambda</span>,
-          <span className="font-bold text-white"> Redis</span>.
-        </p>
-        <p className="pt-4 pl-2 text-primary-300">
-          <span className="font-bold text-white">
-            Desarrollo​ de una aplicación web​
-          </span>{' '}
-          ​y​ de un{' '}
-          <span className="font-bold text-white">
-            ​sistema de gestión de incidencias​
-          </span>{' '}
-          para los trabajadores de mantenimiento del producto. El{' '}
-          <span className="font-bold text-white">sistema de incidencias</span>{' '}
-          se diseño teniendo en mente{' '}
-          <span className="font-bold text-white">integrarse con Pagerduty</span>
-          . Desarrollado con
-          <span className="font-bold text-white"> Kotlin</span>,
-          <span className="font-bold text-white"> Arrow</span>,
-          <span className="font-bold text-white"> Spring Boot</span>,
-          <span className="font-bold text-white"> Javascript</span>,
-          <span className="font-bold text-white"> React</span>,
-          <span className="font-bold text-white"> Clojure</span>,
-          <span className="font-bold text-white"> ​Elasticsearch​</span>,
-          <span className="font-bold text-white"> SNS</span>,
-          <span className="font-bold text-white"> SQS</span>,
-          <span className="font-bold text-white"> Keycloak</span>,
-          <span className="font-bold text-white"> PagerDuty</span>,
-          <span className="font-bold text-white"> PostgreSQL</span>.
-        </p>
+        <ul className="list-inside list-disc pl-2">
+          <li className="pt-2">
+            Implementación de la primera versión del sistema de incidencias de
+            la plataforma.
+          </li>
+          <li className="pt-2">
+            Integración con Firebase para el envío de notificaciones.
+          </li>
+          <li className="pt-2">
+            Integración con Traccar para el sistema de GPS de las bicicletas de
+            la plataforma.
+          </li>
+          <li className="pt-2">
+            Mejora del rendimiento del dashboard de mantenimiento, pasando de
+            una media de 3 segundos por petición a menos de 1 segundo por
+            petición.
+          </li>
+          <li className="pt-2">
+            Mejora del rendimiento de la plataforma a nivel de base de datos,
+            donde determinadas queries de inserción y búsqueda tardaban varios
+            segundos en ejecutarse.
+          </li>
+          <li className="pt-2">
+            Mejora del tiempo de comprobación de si un anclaje estaba libre de 2
+            segundos a 100 milisegundos.
+          </li>
+          <li className="pt-2">
+            Creación de una lambda, en AWS Lambda, para guardar el estado de
+            todos los anclajes y estaciones en Redis.
+          </li>
+          <li className="pt-2">
+            Escribir una guía de cómo desarrollar en local un Lambda con
+            Localstack.
+          </li>
+          <li className="pt-2">
+            Desarrollo inicial de un Docker Compose para trabjar en local del
+            nuevo dashboard de mantenimiento, el cual requería un PostgreSQL,
+            Kong, Konga, Keycloak, Localstack y Redis.
+          </li>
+          <li className="pt-2">
+            Implementación de un Back For Frontend en Kotlin con Spring Boot
+            para el nuevo dashboard de mantenimiento y la aplicación de Android,
+            incluyendo el diseño de la API REST para ambos clientes.
+          </li>
+          <li className="pt-2">
+            Implementación de un microservicio en Kotlin con Spring Boot para el
+            sistema de gestión de incidencias de mantenimiento.
+          </li>
+          <li className="pt-2">
+            Desarrollo de la primera versión del nuevo dashboard de
+            mantenimiento en React.
+          </li>
+          <li className="pt-2">
+            Integración con Pagerduty del microservicio del sistema de gestión
+            de incidencias.{' '}
+          </li>
+          <li className="pt-2">
+            Integración con SQS y SNS del microservicio de gestión de
+            incidencias con el objetivo de evitar saturar la API de Pagerduty,
+            así como la adición de un sistema de reintento, la posibilidad de
+            escalar horizontalmente y una forma de comunicación entre la
+            plataforma y el sistema de gestión de incidencias.
+          </li>
+          <li className="pt-2">
+            Actualización del nuevo dashboard de mantenimiento a React 16.
+          </li>
+          <li className="pt-2">
+            Reducción a la mitad los tiempo de build de la plataforma tras el
+            borrado del código relacionado con el dashboard antiguo.
+          </li>
+        </ul>
       </li>
       <li className="pt-4 md:flex-1/2 lg:flex-3/10">
         <h2 className="text-3xl block">
@@ -177,10 +183,10 @@ const ExperienceContent = () => (
         </span>
         <p className="pt-4 pl-2 text-primary-300">
           <span className="font-bold text-white">
-            Ayudar en el desarrollo de una ​aplicación web de sanidad para
+            Colaboración en el desarrollo de una ​aplicación web de sanidad para
             ayudar a los codificadores​
           </span>{' '}
-          a detectar el diagnóstico, actual y previos de un informe médico y
+          a detectar los diagnósticos, actual y previos de un informe médico y
           todos los procedimientos que se han realizado. Desarrollado con
           <span className="font-bold text-white"> Java 7</span>,
           <span className="font-bold text-white"> MongoDB</span>,
