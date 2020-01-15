464f563121264df8da92349205c7a374b460c83d
Afsoon <said.atrahouch@gmail.com>
2020-01-15 14:42:13 +0100

    removed purge CSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,9 @@
 module.exports = {
-  from: undefined,
   plugins: [
     require("tailwindcss"),
     ...(process.env.NODE_ENV === `production`
       ? [
-          require("@fullhuman/postcss-purgecss")({
-            content: ["./src/**/*.js"],
-            defaultExtractor: content =>
-              content.match(/[A-Za-z0-9-_:/]+/g) || []
-          }),
+          
           require("autoprefixer"),
           require("cssnano")
         ]
