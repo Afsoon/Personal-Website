1f99ba79adf3ba4fd52dde4ce70e2bd1162a19f4
Afsoon <said.atrahouch@gmail.com>
2023-11-07 06:36:25 +0100

    astro init

--- src/components/Layout/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const LayoutPage: React.FunctionComponent = ({ children }) => (
-  <div className="h-screen w-screen flex flex-col lg:flex-row">{children}</div>
-)
-
-export default LayoutPage
--- src/icons/index.tsx
+++
@@ -1,8 +0,0 @@
-export { default as Github } from './Github'
-export { default as IconBriefcase } from './IconBriefcase'
-export { default as IconBuilding } from './IconBuilding'
-export { default as IconGraphBar } from './IconGraphBar'
-export { default as IconHome } from './IconHome'
-export { default as IconMenu } from './IconMenu'
-export { default as IconUser } from './IconUser'
-export { default as Linkedin } from './Linkedin'
